896e899ca302c29be50115d74684d58ef0b7512d
wjq990112 <1163585385@qq.com>
2020-06-14 01:22:56 +0800

    feat: 面试题29. 顺时针打印矩阵

---
+++ sword_to_offer/easy/29.spiral_order.ts
@@ -0,0 +1,48 @@
+/**
+ * @file 面试题29. 顺时针打印矩阵
+ */
+export default function spiralOrder(matrix: number[][]): number[] {
+  let res: Array<number> = [];
+  let mtxLen = matrix.length;
+
+  if (!mtxLen) {
+    return res;
+  }
+
+  let top = 0;
+  let left = 0;
+  let right = matrix[0].length - 1;
+  let bottom = mtxLen - 1;
+
+  while (bottom > top && right > left) {
+    for (let i = left; i < right; i++) {
+      res.push(matrix[top][i]);
+    }
+    for (let i = top; i < bottom; i++) {
+      res.push(matrix[i][right]);
+    }
+    for (let i = right; i > left; i--) {
+      res.push(matrix[bottom][i]);
+    }
+    for (let i = bottom; i > top; i--) {
+      res.push(matrix[i][left]);
+    }
+
+    top++;
+    left++;
+    right--;
+    bottom--;
+  }
+
+  if (top === bottom) {
+    for (let i = left; i <= right; i++) {
+      res.push(matrix[top][i]);
+    }
+  } else if (left === right) {
+    for (let i = top; i <= bottom; i++) {
+      res.push(matrix[i][left]);
+    }
+  }
+
+  return res;
+}
---
+++ sword_to_offer/easy/__tests__/29.spiral_order.ts
@@ -0,0 +1,38 @@
+/**
+ * @file 面试题29. 顺时针打印矩阵
+ */
+import spiralOrder from '../29.spiral_order';
+
+describe('面试题29. 顺时针打印矩阵', () => {
+  it('input: []', () => {
+    expect(spiralOrder([])).toEqual([]);
+  });
+
+  it('input: [[1]]', () => {
+    expect(spiralOrder([[1]])).toEqual([1]);
+  });
+
+  it('input: [[6,9,7]]', () => {
+    expect(spiralOrder([[6, 9, 7]])).toEqual([6, 9, 7]);
+  });
+
+  it(`input: [
+               [1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9]
+             ]`, () => {
+    expect(
+      spiralOrder([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+      ])
+    ).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+  });
+
+  it(`input: [
+               [1, 2, 3, 4],
+               [5, 6, 7, 8],
+               [9, 10, 11, 12]
+             ]`, () => {});
+});
